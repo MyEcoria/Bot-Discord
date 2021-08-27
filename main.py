39f79289bd03570a75ac0823ef548e8c955f8526
Ecoriap <doctorborken@gmail.com>
2021-08-27 19:22:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ async def on_ready():
 async def coucou(ctx):
         server = ctx.guild
         serverDiscordNameCoucou = server.name
-        await ctx.send(f"Coucou jeune **padawan** et bienvenue sur le serveur de **{serverDiscordNameCoucou}")
+        await ctx.send(f"Coucou jeune **padawan** et bienvenue sur le serveur de **{serverDiscordNameCoucou}**")
 
 @bot.command()
 async def bonjour(ctx):
         server = ctx.guild
         serverDiscordNameBonjour = server.name
-        await ctx.send(f"Bonjour jeune **padawan** et bienvenue sur le serveur de **{serverDiscordNameBonjour}")
+        await ctx.send(f"Bonjour jeune **padawan** et bienvenue sur le serveur de **{serverDiscordNameBonjour}**")
 
 @bot.command()
 async def don(ctx):
@@ -28,14 +28,18 @@ async def don(ctx):
         await ctx.send(f"> `{ethereumAdresse}`")
 
 @bot.command()
-async def serverInfo(ctx):
-	server = ctx.guild
-	numberOfTextChannels = len(server.text_channels)
-	numberOfVoiceChannels = len(server.voice_channels)
-	serverDescription = server.description
-	numberOfPerson = server.member_count
-	serverName = server.name
-	message = f"Le serveur **{serverName}** contient *{numberOfPerson}* personnes ! \nLa description du serveur est {serverDescription}. \nCe serveur possède {numberOfTextChannels} salons écrit et {numberOfVoiceChannels} salon vocaux."
-	await ctx.send(message)
+async def commandesInfos(ctx):
+        await ctx.send(f"Les commandes de ce bot son: \n `!coucou` \n `!bonjour` \n `!don` \n `!serverInfo` \n `!commandesInfos`")
 
-bot.run("ODgwNDE1Mzc4NzIxODkwMzM1.YSd8tw.wp0R9ICa2NvIEpqmQaqsgQUhEBw") #Ceci est le token de mon bot. C>
+@bot.command()
+async def serverInfo(ctx):
+        server = ctx.guild
+        numberOfTextChannels = len(server.text_channels)
+        numberOfVoiceChannels = len(server.voice_channels)
+        serverDescription = server.description
+        numberOfPerson = server.member_count
+        serverName = server.name
+        message = f"Le serveur **{serverName}** contient *{numberOfPerson}* personnes ! \nLa description du serveur est {serverDescription}. \nCe serveur possède {numberOfTextChannels} salons écrit et {numberOfVoiceChannels} salon vocaux."    
+        await ctx.send(message)
+
+bot.run("ODgwNDE1Mzc4NzIxODkwMzM1.YSd8tw.avNeUz--C28MHE7-SswyebAjYN8")
